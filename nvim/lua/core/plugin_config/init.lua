a951f7a495ec2c7f9fb432169a7e51222986f5f2
Christopher Murray <chris.murray@calypsoai.com>
2024-03-25 14:43:42 +0000

    adding nvim

---
+++ nvim/lua/core/plugin_config/init.lua
@@ -0,0 +1,9 @@
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.lualine")
+require("core.plugin_config.catppuccin")
+require("core.plugin_config.telescope")
+require("core.plugin_config.treesitter")
+require("core.plugin_config.coc")
+require("core.plugin_config.webdev-icons")
+require("core.plugin_config.nvim-tmux-navigation")
+
---
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,20 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set('n', '<leader>ntf', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<leader>ntt', ':NvimTreeToggle<CR>')
