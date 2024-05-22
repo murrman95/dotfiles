58edd9197d39fd3d2bc6518889bf122626c2b8fe
murrman95 <christopher.murray10@gmail.com>
2024-05-22 07:56:37 -0700

    sets up zsh and omz

--- setup_utils.sh
+++ setup_utils.sh
@@ -14,6 +14,8 @@ if [ "$machine" == "Mac" ]; then
   exit
 fi
 
+start_dir="$(pwd)"
+
 os=$(cat /etc/os-release | grep -o -m 1 "Ubuntu") # m 1 just to get first match and stop
 
 # TODO: have setups for other Linux systems
@@ -31,22 +33,36 @@ if [ "$os" == "Ubuntu" ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   cd ~/.fzf/
   yes | ./install
+  cd "$start_dir"
 
   # neovim setup
   sudo snap install nvim --classic
 
-  echo 'alias vim=nvim' >> ~/.bashrc
-  echo 'alias v=vim' >> ~/.bashrc
 
   # tmux setup
-  sudo apt install tmux
+  yes | sudo apt install tmux
 
   # python
-  sudo add-apt-repository ppa:deadsnakes/ppa
-  sudo apt update
-  sudo apt-get install python3.10 python3.10-dev python3.10-venv
+  yes | sudo add-apt-repository ppa:deadsnakes/ppa
+  yes | sudo apt update
+  ues | sudo apt-get install python3.10 python3.10-dev python3.10-venv
+  
+  # zsh
+  yes | sudo apt install zsh
 
-  source ~/.bashrc
+  # oh-my-zsh and plugins
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  
+  sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="jonathan"/g' ~/.zshrc 
+  sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc 
+  
+  # aliases
+  echo 'alias vim=nvim' >> ~/.zshrc
+  echo 'alias v=vim' >> ~/.zshrc
+
+  echo "Run source ~/.zshrc manually"
 else
   echo "Not Ubuntu detected. os is $os"
 fi
