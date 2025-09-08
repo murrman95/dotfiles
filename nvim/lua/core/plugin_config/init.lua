e37abbd8d89da3df681299ea57ef826c4d3a01e7
Christopher Murray <chris.murray@calypsoai.com>
2025-09-08 14:46:35 -0300

    Syncing for new laptop

--- nvim/lua/core/plugin_config/init.lua
+++ nvim/lua/core/plugin_config/init.lua
@@ -1,6 +1,6 @@
 require("core.plugin_config.nvim-tree")
 require("core.plugin_config.lualine")
-require("core.plugin_config.catppuccin")
+#require("core.plugin_config.catppuccin")
 require("core.plugin_config.telescope")
 require("core.plugin_config.webdev-icons")
 require("core.plugin_config.vim-tmux-navigator")
